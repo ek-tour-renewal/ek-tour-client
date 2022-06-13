f075ecde60dfeb00c22c4120e1425ab55bfa09df
unknown <eodrmfdl1004@naver.com>
2022-06-13 19:38:07 +0900

    modify header and footer

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,24 +3,24 @@ import styles from './header.module.css';
 
 const Header = props => {
   return (
-    <header>
+    <header className={styles.header}>
       <section className={styles.subMenu}>
-        <button className={styles.subMenuButton}>내견적확인</button>
+        <a className={styles.subMenuButton} href=''>내견적확인</a>
         <button className={styles.subMenuButton}>즐겨찾기추가</button>
       </section>
-      <section className={styles.header}>
-        <button className={styles.logoButton}>
+      <nav className={styles.navbar}>
+        <a href=''>
           <img src='http://www.ekhanabus.com/image/ekhana_logo_New3.png' alt='EK tour logo' className={styles.logo} />
-        </button>
-        <nav className={styles.navbar}>
-          <div className={styles.container}>
-            <button className={styles.menu}>회사소개</button>
-            <button className={styles.menu}>버스안내</button>
-            <button className={styles.menu}>견적요청</button>
-            <button className={styles.menu}>고객센터</button>
-          </div>
-        </nav>
-      </section>
+        </a>
+        <section className={styles.container}>
+          <ul className={styles.navbarMenu}>
+            <li><a className={styles.menu} href=''>회사소개</a></li>
+            <li><a className={styles.menu} href=''>버스안내</a></li>
+            <li><a className={styles.menu} href=''>견적요청</a></li>
+            <li><a className={styles.menu} href=''>고객센터</a></li>
+          </ul>
+        </section>
+      </nav>
     </header>
   );
 };
