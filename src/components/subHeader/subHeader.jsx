9e3391e6a99585c4ccb2d0072aab0a1f124d54f4
unknown <eodrmfdl1004@naver.com>
2022-06-18 10:47:26 +0900

    modify sub header

--- src/components/subHeader/subHeader.jsx
+++ src/components/subHeader/subHeader.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './subHeader.module.css';
 
-const SubHeader = (props) => {
+const SubHeader = ({ menu }) => {
   return (
     <section className={styles.subHeader}>
-      <h1 className={styles.title}>CEO 인사말</h1>
+      <h1 className={styles.title}>{menu}</h1>
     </section>
   )
 };
