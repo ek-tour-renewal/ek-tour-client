ed54ddd0bbaac96db1e318982cd0a04f38527446
unknown <eodrmfdl1004@naver.com>
2022-06-18 20:16:00 +0900

    add bus page components

--- src/components/bigBus/bigBus.jsx
+++ src/components/bigBus/bigBus.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './bigBus.module.css';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import Menu from '../menu/menu';
 import SubHeader from '../subHeader/subHeader';
+import Slider from '../slider/slider';
 
-const BigBus = (props) => {
+const BigBus = ({ menu, changeMenu }) => {
+  const ImageList = [
+    { url: './image/bigBus/interior1.jpg' },
+    { url: './image/bigBus/interior2.jpg' },
+    { url: './image/bigBus/interior3.jpg' },
+    { url: './image/bigBus/interior4.jpg' },
+    { url: './image/bigBus/interior5.jpg' },
+    { url: './image/bigBus/interior6.jpg' },
+    { url: './image/bigBus/interior7.jpg' },
+    { url: './image/bigBus/interior8.jpg' },
+    { url: './image/bigBus/interior9.jpg' },
+    { url: './image/bigBus/interior10.jpg' },
+    { url: './image/bigBus/interior11.jpg' },
+    { url: './image/bigBus/interior12.jpg' },
+    { url: './image/bigBus/interior13.jpg' },
+  ];
+
+  useEffect(() => {
+    changeMenu('45인승 대형');
+    console.log(menu);
+  }, []);
+
   return (
-    <main>
+    <main className={styles.bigBus}>
       <Header />
       <section className={styles.main}>
         <section className={styles.sideMenu}>
           <Menu />
         </section>
         <section className={styles.mainDetail}>
-          <SubHeader />
-          <section>
-            <h1>45인승 대형</h1>
+          <SubHeader menu={menu} />
+          <h1 className={styles.bigBusTitle}>45인승 대형</h1>
+          <section className={styles.container}>
+            <p className={styles.busDetail}>
+              가장 일반적으로 많이 애용되는 전세버스입니다. <br />
+              40~45인승의 단체 여행에 적합하고 기업체, 관공서, 동호회, 일반단체의 국내 버스여행에 <br />
+              많이 이용되며 기업체와 관공서 출퇴근 차량, 대학생 MT, OT, 외국인 투어에도 많이 이용됩니다.
+            </p>
+            <img src="./image/bigBusExterior.jpg" alt="45인승 대형버스 외관" />
+            <h2 className={styles.busImageTitle}>차량 사진</h2>
+            <Slider imageList={ImageList}/>
           </section>
         </section>
       </section>
--- src/components/limousine/limousine.jsx
+++ src/components/limousine/limousine.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './limousine.module.css';
 import Header from '../header/header';
 import Menu from '../menu/menu';
 import SubHeader from '../subHeader/subHeader';
 import Footer from '../footer/footer';
+import Slider from '../slider/slider';
 
-const Limousine = (props) => {
+const Limousine = ({ menu, changeMenu }) => {
+  const ImageList = [
+    { url: './image/limousine/interior1.jpg' },
+    { url: './image/limousine/interior2.jpg' },
+    { url: './image/limousine/interior3.jpg' },
+    { url: './image/limousine/interior4.jpg' },
+    { url: './image/limousine/interior5.jpg' },
+    { url: './image/limousine/interior6.jpg' },
+    { url: './image/limousine/interior7.jpg' },
+    { url: './image/limousine/interior8.jpg' },
+    { url: './image/limousine/interior9.jpg' },
+    { url: './image/limousine/interior10.jpg' },
+    { url: './image/limousine/interior11.jpg' },
+  ];
+
+  useEffect(() => {
+    changeMenu('28인승 리무진');
+    console.log(menu);
+  }, []);
+
   return (
-    <main>
+    <main className={styles.limousine}>
       <Header />
       <section className={styles.main}>
         <section className={styles.sideMenu}>
           <Menu />
         </section>
         <section className={styles.mainDetail}>
-          <SubHeader />
-          <section>
-            <h1>28인승 리무진</h1>
+          <SubHeader menu={menu} />
+          <h1 className={styles.limousineTitle}>28인승 리무진</h1>
+          <section className={styles.container}>
+            <p className={styles.limousineDetail}>
+              최고급의 편의 시설과 안락한 시트, 각종 고급형 시설을 갖춘 리무진으로서 <br />
+              24인승 ~ 28인승의 단체 여행에 이용됩니다. 고급 골프모임, 동호회, 관공서, 기업체, <br />
+              일반인 모두 이용할 수 있으며 최고급형 대형 버스입니다.
+            </p>
+            <img src="./image/limousineExterior.jpg" alt="28인승 리무진 외관" />
+            <h2 className={styles.busImageTitle}>차량 사진</h2>
+            <Slider imageList={ImageList}/>
           </section>
         </section>
       </section>
