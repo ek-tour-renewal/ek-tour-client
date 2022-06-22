22284df81f783fe775d01fda2b3bf1cbdbdd8643
unknown <eodrmfdl1004@naver.com>
2022-06-22 18:56:53 +0900

    add function

--- src/service/ekTour.js
+++ src/service/ekTour.js
@@ -12,11 +12,30 @@ class Ektour {
     return response;
   }
 
-  async RequestData(pageNumber) {
+  async updateData(data) {
+    const response = await this.ektour.put('/estimate/search/my', data);
+    return response;
+  }
+
+  async getAllPageCount() {
+    const response = await this.ektour.get('/estimate/all/page',{});
+    return response;
+  }
+
+  async requestData(pageNumber) {
     const response = await this.ektour.get('/all', {
       params: {
         page: pageNumber,
       },
+    });
+    return response;
+  }
+
+  async getMyEstimate(data) {
+    const { phone, password } = data;
+    const response = await this.ektour.post(`/estimate/search/my`, {
+      phone: phone,
+      password: password,
     });
     return response;
   }
