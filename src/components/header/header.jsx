35e9af91bac6647247e4b0abe36adbc8b8955b32
unknown <eodrmfdl1004@naver.com>
2022-06-30 13:24:38 +0900

    add slide request estimate

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MyEstimateV1 from '../myEstimate/myEstimateV1';
+import RequestEstimateSlide from '../requestEstimate/requestEstimateSlide';
 import styles from './header.module.css';
 
 const Header = (props) => {
   const [myOpen, setMyOpen] = useState(false);
+  const [requestOpen, setRequestOpen] = useState(false);
   const navigate = useNavigate();
 
   const onNavigate = (path) => {
@@ -13,6 +15,8 @@ const Header = (props) => {
 
   const handleClickMyEstimate = () => { setMyOpen(true); }
   const handleCloseMyEstimate = () => { setMyOpen(false); }
+  const handleOpenRequestEstimate = () => { setRequestOpen(true); };
+  const handleCloseRequestEstimate = () => { setRequestOpen(false); };
 
   return (
     <header className={styles.header}>
@@ -22,9 +26,16 @@ const Header = (props) => {
         </span>
         <div className={styles.navbarContainer}>
           <ul className={styles.navbarMenu}>
-            {/* 슬라이딩패널 */}
             <li className={styles.request}>
-              <button className={styles.menuTitle}>견적요청하기</button>
+              <button className={styles.menuTitle} onClick={handleOpenRequestEstimate}>견적요청하기</button>
+              <RequestEstimateSlide
+                handleCloseRequestEstimate={handleCloseRequestEstimate}
+                open={requestOpen}
+                menu={props.menu}
+                changeMenu={props.changeMenu}
+                Ref={props.Ref}
+                getData={props.getData}
+              />
             </li>
             <li className={styles.myEstimate}>
               <button className={styles.menuTitle} onClick={handleClickMyEstimate}>나의견적확인</button>
