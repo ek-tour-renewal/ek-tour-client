90d30d9f792969fb5ad390b25a5dd8af1a04d31e
unknown <eodrmfdl1004@naver.com>
2022-07-01 12:19:33 +0900

    update header button and side menu animation

--- src/components/sideMenu/sideMenu.jsx
+++ src/components/sideMenu/sideMenu.jsx
@@ -1,4 +1,4 @@
-import { Grid, Button } from '@mui/material';
+import { Button } from '@mui/material';
 import ApartmentIcon from '@mui/icons-material/Apartment';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
@@ -9,53 +9,32 @@ import { useNavigate } from 'react-router-dom';
 import styles from './sideMenu.module.css';
 
 const SideMenu = (props) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const onNavigate = (path) => {
-    navigate(path);
-  };
+  // const onNavigate = (path) => {
+  //   navigate(path);
+  // };
 
   return (
     <div className={styles.sideMenu}>
       <Button
         variant='contained'
-        sx={{
-          height: '6em', color: '#5A4231', borderRadius: '20px', marginBottom: '10px', backgroundColor: 'rgba(255, 250, 203, 0.7)',
-          display: 'flex', flexDirection: 'column',
-          '&:hover': {
-            backgroundColor: '#FCFCFC',
-            color: 'EC9F46',
-          },
-        }}
-        onClick={() => onNavigate('/introduce')}>
+        className={styles.button}
+        >
         <ApartmentIcon fontSize='large' />
         회사소개
       </Button>
       <Button
         variant='contained'
-        sx={{
-          height: '6em', color: '#5A4231', borderRadius: '20px', marginBottom: '10px', backgroundColor: 'rgba(255, 250, 203, 0.7)',
-          display: 'flex', flexDirection: 'column',
-          '&:hover': {
-            backgroundColor: '#FCFCFC',
-            color: 'EC9F46',
-          },
-        }}
-        onClick={() => onNavigate('/bus')}>
+        className={styles.button}
+        >
         <DirectionsBusIcon fontSize='large' />
         버스안내
       </Button>
       <Button
         variant='contained'
-        sx={{
-          height: '6em', color: '#5A4231', borderRadius: '20px', backgroundColor: 'rgba(255, 250, 203, 0.7)',
-          display: 'flex', flexDirection: 'column',
-          '&:hover': {
-            backgroundColor: '#FCFCFC',
-            color: 'EC9F46',
-          },
-        }}
-        onClick={() => onNavigate('/request')}>
+        className={styles.button}
+      >
         <ReceiptLongIcon fontSize='large' />
         견적요청
       </Button>
